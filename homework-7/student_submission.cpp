49ef8068193a11b3890035f8dc0fa64f2680f1ca
Fabio Gaiba <faga2703@gmail.com>
2024-06-23 14:33:06 +0200

    Added tester to homework7, might not work out of the box but has the command for running open mpi

--- homework-7/student_submission.cpp
+++ homework-7/student_submission.cpp
@@ -8,39 +8,49 @@
 #include "life.h"
 #include "Utility.h"
 #include "VideoOutput.h"
+#include <mpi.h>
 
 /*
   Apply the game of life rules on a Torus --> grid contains shadow rows and columns
   to simplify application of rules i.e. grid actually ranges from grid [ 1.. height - 2 ][ 1 .. width - 2]
 */
-void evolve(ProblemData& problemData) {
-    auto& grid = *problemData.readGrid;
-    auto& writeGrid = *problemData.writeGrid;
+void evolve(ProblemData &problemData)
+{
+    auto &grid = *problemData.readGrid;
+    auto &writeGrid = *problemData.writeGrid;
     // For each cell
-    for (int i = 1; i < GRID_SIZE - 1; i++) {
-        for (int j = 1; j < GRID_SIZE - 1; j++) {
+    for (int i = 1; i < GRID_SIZE - 1; i++)
+    {
+        for (int j = 1; j < GRID_SIZE - 1; j++)
+        {
             // Calculate the number of neighbors
             int sum = grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] +
-                grid[i][j - 1] + grid[i][j + 1] +
-                grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1];
+                      grid[i][j - 1] + grid[i][j + 1] +
+                      grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1];
 
-            if (!grid[i][j]) {
+            if (!grid[i][j])
+            {
                 // If a cell is dead, it can start living by reproduction or stay dead
-                if (sum == 3) {
+                if (sum == 3)
+                {
                     // reproduction
                     writeGrid[i][j] = true;
                 }
-                else {
+                else
+                {
                     writeGrid[i][j] = false;
                 }
             }
-            else {
+            else
+            {
                 // If a cell is alive, it can stay alive or die through under/overpopulation
-                if (sum == 2 || sum == 3) {
+                if (sum == 2 || sum == 3)
+                {
                     // stays alive
                     writeGrid[i][j] = true;
                 }
-                else {
+                else
+                {
                     // dies due to under or overpopulation
                     writeGrid[i][j] = false;
                 }
@@ -53,15 +63,18 @@ void evolve(ProblemData& problemData) {
   Copies data from the inner part of the grid to
   shadow (padding) rows and columns to transform the grid into a torus.
 */
-void copy_edges(bool(&grid)[GRID_SIZE][GRID_SIZE]) {
+void copy_edges(bool (&grid)[GRID_SIZE][GRID_SIZE])
+{
     // Copy data to the boundaries
-    for (int i = 1; i < GRID_SIZE - 1; i++) {
+    for (int i = 1; i < GRID_SIZE - 1; i++)
+    {
         // join rows together
         grid[i][0] = grid[i][GRID_SIZE - 2];
         grid[i][GRID_SIZE - 1] = grid[i][1];
     }
 
-    for (int j = 1; j < GRID_SIZE - 1; j++) {
+    for (int j = 1; j < GRID_SIZE - 1; j++)
+    {
         // join columns together
         grid[0][j] = grid[GRID_SIZE - 2][j];
         grid[GRID_SIZE - 1][j] = grid[1][j];
@@ -74,38 +87,24 @@ void copy_edges(bool(&grid)[GRID_SIZE][GRID_SIZE]) {
     grid[GRID_SIZE - 1][0] = grid[1][GRID_SIZE - 2];
 }
 
-int main(int argc, char** argv) {
-    bool activateVideoOutput = false;
-    if (argc > 1) {
-        if (argc == 2 && strcmp(argv[1], "-g") == 0) {
-            activateVideoOutput = true;
-        }
-        else {
-            fprintf(stderr, "Usage:\n  %s [-g]\n    -g: Activate graphical output.\n", argv[0]);
-            exit(153);
-        }
-    }
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
 
-    auto* problemData = new ProblemData;
+    auto *problemData = new ProblemData;
 
     // As with Jack Sparrow's exercise, this needs FFMPEG.
     // As an alternative, you can write individual png files to take a look at the data.
-    if (activateVideoOutput) {
-        VideoOutput::beginVideoOutput();
-        VideoOutput::saveToFile(*problemData->readGrid, "grid_beginning.png");
-    }
 
     Utility::readProblemFromInput(*problemData);
 
-    //TODO@Students: This is the main simulation. Parallelize it using MPI.
-    for (int iteration = 0; iteration < NUM_SIMULATION_STEPS; ++iteration) {
+    // TODO@Students: This is the main simulation. Parallelize it using MPI.
+    for (int iteration = 0; iteration < NUM_SIMULATION_STEPS; ++iteration)
+    {
         copy_edges(*problemData->readGrid);
 
-        if (activateVideoOutput) {
-            VideoOutput::writeVideoFrames(*problemData);
-        }
-
-        if (iteration % SOLUTION_REPORT_INTERVAL == 0) {
+        if (iteration % SOLUTION_REPORT_INTERVAL == 0)
+        {
             Utility::outputIntermediateSolution(iteration, *problemData);
         }
 
@@ -116,12 +115,7 @@ int main(int argc, char** argv) {
 
     Utility::outputSolution(*problemData);
 
-    if (activateVideoOutput) {
-        VideoOutput::endVideoOutput();
-        VideoOutput::saveToFile(*problemData->readGrid, "grid_final.png");
-    }
-
     delete problemData;
+    MPI_Finalize();
     return 0;
 }
-
---
+++ homework-7/tester.py
@@ -0,0 +1,46 @@
+import time
+import subprocess
+
+
+def run_command(command):
+    try:
+        # Use shell=True to run the command in the shell
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
+        # Wait for the process to complete and capture the output and error
+        stdout, stderr = process.communicate()
+
+        # Check if the process terminated with a non-zero exit code
+        if process.returncode != 0:
+            print(f"Command failed with return code {process.returncode}")
+            return (stderr)
+        else:
+            # Print the captured output
+            return (stdout)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+seed = 12345
+
+
+seq_cmd = f"echo {seed} | ./sequential_implementation"
+par_cmd = f"echo {seed} |  mpirun ./student_submission -np=16 --oversubscribe"
+
+start = time.time()
+seq_res = run_command(seq_cmd)
+mid = time.time()
+par_res = run_command(par_cmd)
+end = time.time()
+
+if (seq_res != par_res):
+    print(f"FAILED")
+    print(seq_res)
+    print(par_res)
+else:
+    print(f"PASSED")
+
+print(start, mid, end)
+print(f"SPEEDUP is (not reliable on my machine) {(mid-start)/(end-mid)}")
